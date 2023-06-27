1919a612b5edc80de9002a1fde006980c9f1c556
RaffaAraujo <Rafael.Araujo@copart.com>
2023-06-27 18:00:25 -0300

    create a perfil page

--- src/screens/Settings/index.tsx
+++ src/screens/Settings/index.tsx
@@ -1,19 +1,104 @@
 import React from "react";
 
-import { Container } from "./styles";
 import Button from "../../components/Button";
 import { useAuth } from "../../hooks/auth";
+import { KeyboardAvoidingView, Platform, ScrollView, View } from "react-native";
+import { Input } from "../../components/Input";
+
+import { Feather } from "@expo/vector-icons";
+
+import {
+  Container,
+  BackButton,
+  Title,
+  UserAvatarButton,
+  UserAvatar,
+} from "./styles";
+import { useNavigation } from "@react-navigation/native";
 
 export function Settings() {
-  const { signOut } = useAuth();
+  const { signOut, user } = useAuth();
+  const navigation = useNavigation();
 
   const handleOnLogout = () => {
     signOut();
   };
 
   return (
-    <Container>
-      <Button onPress={() => handleOnLogout()} text="Logout" />
-    </Container>
+    <>
+      <KeyboardAvoidingView
+        style={{ flex: 1 }}
+        behavior={Platform.OS === "ios" ? "padding" : undefined}
+        enabled
+      >
+        <ScrollView
+          keyboardShouldPersistTaps="handled"
+          contentContainerStyle={{ flex: 1 }}
+        >
+          <Container>
+            <BackButton onPress={() => navigation.navigate("home")}>
+              <Feather name="chevron-left" color="#fff" size={24} />
+            </BackButton>
+
+            <View
+              style={{
+                alignItems: "center",
+                justifyContent: "flex-end",
+              }}
+            >
+              <UserAvatarButton>
+                <Feather name="user" color="#fff" size={40} />
+              </UserAvatarButton>
+              <Title>Meu perfil</Title>
+            </View>
+
+            <View>
+              <Input
+                autoCapitalize="words"
+                icon="user"
+                placeholder="Nome"
+                returnKeyType="next"
+                value={user.name}
+                editable={false}
+              />
+
+              <Input
+                secureTextEntry
+                icon="lock"
+                placeholder="Senha atual"
+                textContentType="newPassword"
+                returnKeyType="next"
+                editable={false}
+                style={{ opacity: 0.2 }}
+              />
+              <Input
+                secureTextEntry
+                icon="lock"
+                placeholder="Nova senha"
+                textContentType="newPassword"
+                returnKeyType="next"
+                editable={false}
+                style={{ opacity: 0.2 }}
+              />
+              <Input
+                secureTextEntry
+                icon="lock"
+                placeholder="Confirmar nova senha"
+                textContentType="newPassword"
+                returnKeyType="send"
+                editable={false}
+                style={{ opacity: 0.2 }}
+              />
+
+              <Button
+                text="Confirmar mudanças"
+                disabled
+                style={{ opacity: 0.2 }}
+              />
+            </View>
+          </Container>
+        </ScrollView>
+      </KeyboardAvoidingView>
+    </>
   );
 }
--- src/screens/Settings/styles.ts
+++ src/screens/Settings/styles.ts
@@ -1,10 +1,42 @@
-import styled from 'styled-components/native';
+import styled from "styled-components/native";
+import { Platform } from "react-native";
 
 export const Container = styled.View`
+  padding: 0 30px ${Platform.OS === "android" ? 30 : 30}px;
+
   flex: 1;
+  background: ${({ theme }) => theme.colors.gray_800};
+`;
+
+export const BackButton = styled.TouchableOpacity`
+  margin-top: 40px;
+`;
+
+export const Title = styled.Text`
+  font-size: 20px;
+  color: #f4ede8;
+  font-family: "Inter_500Medium";
+  margin: 24px 0;
+`;
+
+export const UserAvatarButton = styled.TouchableOpacity`
+  width: 80px;
+  height: 80px;
+  border-radius: 96px;
+
   align-items: center;
   justify-content: center;
 
-  background: ${({ theme }) => theme.colors.gray_100};
-  color: ${({ theme }) => theme.colors.gray_300};
+  background-color: red;
+  margin-left: 12px;
+`;
+
+export const UserAvatar = styled.View`
+  width: 186px;
+  height: 186px;
+  border-radius: 96px;
+
+  align-items: center;
+  justify-content: center;
+  background-color: red;
 `;
